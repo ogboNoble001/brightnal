706f0249030a648c4090efbbcdc77818abed279a
ogboNoble001 <ogbonoble84@gmail.com>
2025-11-24 20:00:04 +0100

    update index.js

--- frontend/JS_SCRIPTS/index.js
+++ frontend/JS_SCRIPTS/index.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   if (typeof AOS !== "undefined") {
     AOS.init({
-      offset: 200,
+      offset: 150,
       delay: 0,
       easing: 'ease-in-out',
       once: false,
