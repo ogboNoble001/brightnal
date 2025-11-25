c5b31e92ea6abb7f3c98ae598c55e81c11dddb19
ogboNoble001 <ogbonoble84@gmail.com>
2025-11-25 21:15:37 +0100

    update linkTracker.js

--- frontend/util/JS_SCRIPTS/linkTracker.js
+++ frontend/util/JS_SCRIPTS/linkTracker.js
@@ -3,45 +3,24 @@ export function initNotchNav(navSelector) {
     
     const currentPath = window.location.pathname.split('/').filter(Boolean).pop() || 'explore';
     
-
-    const activeItem = Array.from(navItems).find(item => 
+    const activeItem = Array.from(navItems).find(item =>
         item.getAttribute('data-url') === currentPath
     );
     
-
     if (activeItem) {
         setActive(activeItem, navItems);
     }
     
-
     navItems.forEach((item) => {
         item.addEventListener('click', () => {
             const url = item.getAttribute('data-url');
             
-
-            window.history.pushState({}, '', `/${url}`);
-            
-            setActive(item, navItems);
-            
-
-            window.dispatchEvent(new CustomEvent('navigationChange', { detail: { url } }));
+            // Simply navigate to the new page (full reload)
+            window.location.href = `/${url}`;
         });
     });
     
-
-    window.addEventListener('popstate', () => {
-        const newPath = window.location.pathname.split('/').filter(Boolean).pop() || 'explore';
-        const newActiveItem = Array.from(navItems).find(item => 
-            item.getAttribute('data-url') === newPath
-        );
-        
-        if (newActiveItem) {
-            setActive(newActiveItem, navItems);
-        }
-    });
-    
     function setActive(activeItem, allItems) {
-
         allItems.forEach(i => {
             const span = i.querySelector('span');
             const svg = i.querySelector('svg');
@@ -50,7 +29,6 @@ export function initNotchNav(navSelector) {
             if (svg) svg.setAttribute('stroke', '#BDBEBD');
         });
         
-
         const activeSpan = activeItem.querySelector('span');
         const activeSvg = activeItem.querySelector('svg');
         
