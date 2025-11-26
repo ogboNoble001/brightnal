d9c8544a0bdac8382fc93a0cb12513b0a16258a2
ogboNoble001 <ogbonoble84@gmail.com>
2025-11-26 18:35:49 +0100

    update handleClick_SwitchPg.js

--- frontend/util/JS_SCRIPTS/handleClick_SwitchPg.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('click', (e) => {
-  const el = e.target.closest('[data-url]');
-  if (!el) return;
-  const url = el.dataset.url;
-  window.location.href = `/${url}`;
-});
