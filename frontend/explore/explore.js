ced2b1a19291f8ba1fca17917234c7ba541fe803
ogboNoble001 <ogbonoble84@gmail.com>
2025-11-26 14:45:14 +0100

    update

--- frontend/explore/explore.js
+++ frontend/explore/explore.js
@@ -60,7 +60,8 @@ function createImageCard(item) {
     card.addEventListener('click', () => {
     overlay.classList.add('active');
     overlay.innerHTML = `
-        <div class="back-btn"></div>
+    <div class="back-btn">
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left-icon lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg></div>
         <div class="image-wrapper">
         <img src='${item.src}' />
 </div>
