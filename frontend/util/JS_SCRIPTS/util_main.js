7ec45629b33486a1c6d2a79418149203c00f96fd
ogboNoble001 <ogbonoble84@gmail.com>
2025-11-28 09:21:24 +0100

    update

--- frontend/util/JS_SCRIPTS/util_main.js
+++ frontend/util/JS_SCRIPTS/util_main.js
@@ -2,11 +2,18 @@ window.addEventListener('DOMContentLoaded', () => {
         const overlay = document.querySelector('.overlay-div');
         const inputPrnt = document.querySelector('.inputPrnt');
         const closeBtn = inputPrnt.querySelector('.closeBtn');
-        
+        const input =  document.querySelector('.input')
+        input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+    }
+});
         if (!overlay || !closeBtn) return;
         
         closeBtn.addEventListener('click', (e) => {
-                e.stopPropagation(); // Prevent click from bubbling to inputPrnt
+                input.value=''
+                input.blur()
+                e.stopPropagation(); // 
                 overlay.classList.remove('active');
                 closeBtn.classList.remove('active');
         });
